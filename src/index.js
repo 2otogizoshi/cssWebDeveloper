d66553505cb918deb95eb6854825429ee7a3ef03
2otogizoshi <47193542+2otogizoshi@users.noreply.github.com>
2019-02-04 12:24:08 -0600

    element class added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-//ms css web developer 
+//ms css web developer series
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
+
 <h1>Hello Vanilla!</h1>
+<!-- element -->
 <div>
   We use Parcel to bundle this sandbox, you can find more info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+  <div class="title">Title</ div>
+  <div class="title" id="green">element</ div>  
 </div>
 `;
